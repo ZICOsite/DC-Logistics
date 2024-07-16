8d3d33eb0afdcf9c7c0484205dc63661dc3d5419
ZICO <ziyodahmedov23@gmail.com>
2024-07-16 12:04:08 +0500

    add close icon for modal and fix height picture

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,7 @@ setTimeout(() => {
   <Teleport to="#zico">
     <Modal v-if="modal" @click="modal = false">
       <div class="modal__content" @click.stop>
+        <span class="modal__content-close" @click="modal = false"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="#fff" d="M13.46 12L19 17.54V19h-1.46L12 13.46L6.46 19H5v-1.46L10.54 12L5 6.46V5h1.46L12 10.54L17.54 5H19v1.46z"/></svg></span>
         <h2 class="modal__title">Оставить заявку</h2>
         <p class="modal__txt">
           Наши менеджеры <span class="modal__span">свяжутся с вами!</span>
